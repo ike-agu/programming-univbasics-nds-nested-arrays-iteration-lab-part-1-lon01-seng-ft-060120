fb5cd63f15e5e83552993039ca32ad32c04e6860
ikenna agulobi <ikennaww@yahoo.fr>
2020-04-21 02:24:26 +0000

    Automatically backed up by Learn

--- lib/iteration_with_loops.rb
+++ lib/iteration_with_loops.rb
@@ -6,11 +6,11 @@ def find_even_values(src)
   
   row_index = 0 
   
-  while row_index < src.length do 
+  while row_index < src.size do 
     
     element_index = 0 
     
-    while element_index < src[row_index].length do
+    while element_index < src[row_index].size do
       if  src[row_index][element_index].even?
         p src[row_index][element_index]
 end 
